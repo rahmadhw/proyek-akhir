47dbbadfd063d00ca0165fe08414e96b22f0a736
rahmadhw <rahmadhidyat098@gmail.com>
2022-05-27 15:38:58 +0700

    upgrading

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,6 @@
 const hero = document.querySelector(".section-deskripsi");
 const anggota = document.querySelector(".anggota");
+const judul_kerja = document.querySelector(".judul_kerja_kelompok");
 
 window.onscroll = function() {Scrolling()};
 
@@ -13,6 +14,11 @@ function Scrolling(){
         hero.classList.add("test");
         if (document.documentElement.scrollTop >= 918) {
             anggota.classList.add("test");
+            if (document.documentElement.scrollTop >= 2123) {
+                judul_kerja.classList.add("test");
+            }else {
+                judul_kerja.classList.remove("test");
+            }
         }else {
             anggota.classList.remove("test");
         }
